42469e0fb2037a72d229fb9a7ec3beb70583ed93
Egor Zaretskiy <gorcer@gmail.com>
2013-12-09 22:38:30 +1100

    fix

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+
 //$config=dirname(__FILE__).'/protected/config/main.php';
 
 if ($_SERVER['HTTP_HOST']=='btcbot.loc')
 {
+	$yii=dirname(__FILE__).'/../yii/framework/yii.php';
 	$config = dirname(__FILE__).'/protected/config/main_local.php';
 	defined('YII_DEBUG') or define('YII_DEBUG',true);
 	defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 }
 else
+{
+	$yii=dirname(__FILE__).'../../yii/framework/yii.php';
 	$config=dirname(__FILE__).'/protected/config/main.php';
+}
 
 // remove the following lines when in production mode
 // defined('YII_DEBUG') or define('YII_DEBUG',true);
