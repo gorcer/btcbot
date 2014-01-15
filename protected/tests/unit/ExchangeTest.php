b80cdabde609c2a998d38ba61549b17a4c4b8e13
gorcer <gorcer@gmail.com>
2014-01-15 17:23:25 +1100

    fix

--- protected/tests/unit/ExchangeTest.php
+++ protected/tests/unit/ExchangeTest.php
@@ -1,12 +1,12 @@
 <?php
 
-class ExchangeTest extends CTestCase {
+class ExchangeTest extends CDbTestCase {
 
 	public $fixtures=array(
 			'exchange'=>'Exchange',
 	);
 	
-	public function testGetDataFrom($dt)
+	public function testGetDataFrom()
 	{
 		$list = $this->exchange->getDataFrom('2013-01-01 02:55:00');
 		$this->assertEquals('2013-01-01 03:00:00', $list[0]['dtm']); 
