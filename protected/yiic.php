7ebf7c8178024737717e0f77c4afb88a92485919
egor zaretskiy <gorcer@gmail.com>
2014-02-11 14:59:17 +1100

    fix

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,13 @@
 <?php
 
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
+//$yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
+$yii_path = '/var/lib/yii';
+//$yii_path=dirname(__FILE__).'/../../../yii';
+
+$yiic=$yii_path.'/framework/yiic.php';
+
+
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
