f7cb5e80d6fc8f5c4a6e6220ed634c686886f1fd
egor zaretskiy <gorcer@gmail.com>
2014-02-11 15:01:48 +1100

    fix

--- protected/console.php
+++ protected/console.php
@@ -1,7 +1,7 @@
 <?php
 
 $yii=dirname(__FILE__).'/../../yii/framework/yii.php';
-$config=dirname(__FILE__).'/config/console.php'; // конфиг для консолек который выше описан
+$config=dirname(__FILE__).'/config/test.php'; // конфиг для консолек который выше описан
 
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true); // включаем дебаг
