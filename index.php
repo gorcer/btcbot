8530bc52233fd1cfb2e01fa24ed86a111b6dae3f
Egor Zareckiy <gorcer@drom.ru>
2017-12-01 10:59:50 +1000

    fix

--- index.php
+++ index.php
@@ -14,16 +14,18 @@ ini_set('error_reporting', E_ALL);
 //defined('YII_DEBUG') or define('YII_DEBUG',true);
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
+require(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
+require(dirname(dirname(__FILE__)) . '/vendor/yiisoft/yii/framework/yiilite.php');
 
 if ($_SERVER['HTTP_HOST']=='btcbot.gorcer.com' || $_SERVER['HTTP_HOST']=='btcbot.gorcer.my')
 {
-	$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+
 	$config=dirname(__FILE__).'/protected/config/main.php';
 	define('YII_DEBUG',false);
 }
 else
 {	
-	$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+
 	$config = dirname(__FILE__).'/protected/config/main_local.php';
 	define('YII_DEBUG',true);
 	defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
@@ -34,5 +36,4 @@ else
 // specify how many levels of call stack should be shown in each log message
 // defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-require_once($yii);
 Yii::createWebApplication($config)->run();
--- protected/yiic.php
+++ protected/yiic.php
@@ -1,14 +1,19 @@
 <?php
 
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
+//$yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
 //$yii_path = '/var/lib/yii';
 //$yii_path=dirname(__FILE__).'/../../../yii';
 
 //$yiic=$yii_path.'/framework/yiic.php';
 
-
 //$config=dirname(__FILE__).'/config/test.php';
+
+
+require(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
+require(dirname(dirname(__FILE__)) . '/vendor/yiisoft/yii/framework/yiilite.php');
+
+
 $config=dirname(__FILE__).'/config/main.php';
 
-require_once($yiic);
+
